35440d91abeb7d06d8ac9a6322e1f0976ee3611b
dimanux <dimanux@gmail.com>
2015-12-25 22:28:40 +0300

    Force immersive/fullscreen mode on any android device

--- src/android/com/mesmotronic/plugins/FullScreenPlugin.java
+++ src/android/com/mesmotronic/plugins/FullScreenPlugin.java
@@ -307,12 +307,6 @@ public class FullScreenPlugin extends CordovaPlugin
 	 */
 	protected boolean immersiveMode()
 	{
-		if (!isImmersiveModeSupported())
-		{
-			context.error("Not supported");
-			return false;
-		}
-		
 		activity.runOnUiThread(new Runnable()
 		{
 			@Override
@@ -322,13 +316,19 @@ public class FullScreenPlugin extends CordovaPlugin
 				{
 					resetWindow();
 					
-					final int uiOptions = 
+					final int uiOptions = ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) ?
 						View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
 						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
 						| View.SYSTEM_UI_FLAG_FULLSCREEN
-						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+						| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+						:
+						View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+						| View.SYSTEM_UI_FLAG_FULLSCREEN
+						| View.SYSTEM_UI_FLAG_LOW_PROFILE
+						);
 					
 					window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 					decorView.setSystemUiVisibility(uiOptions);
